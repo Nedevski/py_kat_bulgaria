0ecdca17ad0389003b655735ac274a2a42eee30c
Nikola Nedevski <nikola.nedevski@mentormate.com>
2023-04-06 11:58:19 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = Path("README.md").read_text()
 
 setup(
     name="kat_bulgaria",
-    version="0.2.3",
+    version="0.3.0",
     description="A library to check for existing obligations to KAT Bulgaria",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -16,7 +16,7 @@ setup(
     author_email="nikola.nedevski@gmail.com",
     license="MIT",
     packages=find_packages(include=["kat_bulgaria"]),
-    install_requires=["requests"],
+    install_requires=["httpx"],
     setup_requires=["pytest-runner"],
     tests_require=["pytest==4.4.1"],
     test_suite="tests",
