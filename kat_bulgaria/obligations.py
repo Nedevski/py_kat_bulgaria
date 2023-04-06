b4d4ea68da8ad47c056cea83223d789348c0c253
Nikola Nedevski <nikola.nedevski@mentormate.com>
2023-04-06 11:25:50 +0300

    Finished refactoring

--- kat_bulgaria/obligations.py
+++ kat_bulgaria/obligations.py
@@ -1,22 +1,54 @@
 """Obligations module"""
 
 import re
-import asyncio
 import httpx
+
 
 _REQUEST_TIMEOUT = 5
 _KAT_OBLIGATIONS_URL = "https://e-uslugi.mvr.bg/api/Obligations/AND?mode=1&obligedPersonIdent={egn}&drivingLicenceNumber={license_number}"
-_HAS_NON_HANDED_SLIP = "hasNonHandedSlip"
+
+_RESP_HAS_NON_HANDED_SLIP = "hasNonHandedSlip"
+_RESP_OBLIGATIONS = "obligations"
 
 REGEX_EGN = r"^[0-9]{2}[0,1,2,4][0-9][0-9]{2}[0-9]{4}$"
 REGEX_DRIVING_LICENSE = r"^[0-9]{9}$"
 
 
+class KatObligationsSimpleResponse:
+    """The obligations response object."""
+
+    def __init__(self, data: any) -> None:
+        self.has_obligations = (
+            data[_RESP_HAS_NON_HANDED_SLIP] or len(data[_RESP_OBLIGATIONS]) > 0
+        )
+
+
 class KatObligationsResponse:
     """The obligations response object."""
 
-    def __init__(self, has_obligations: bool) -> None:
-        self.has_obligations = has_obligations
+    def __init__(self, data: any) -> None:
+        self.obligations = []
+        self.has_non_handed_slip = data[_RESP_HAS_NON_HANDED_SLIP]
+        self.has_obligations = (
+            data[_RESP_HAS_NON_HANDED_SLIP] or len(data[_RESP_OBLIGATIONS]) > 0
+        )
+
+        if len(data[_RESP_OBLIGATIONS]) > 0:
+            for obligation in data[_RESP_OBLIGATIONS]:
+                self.obligations.append(
+                    {
+                        "amount": obligation["amount"],
+                        "description": obligation["paymentReason"],
+                        "documentNumber": obligation["additionalData"][
+                            "documentNumber"
+                        ],
+                        "date_created": obligation["additionalData"]["fishCreateDate"],
+                        "date_served": obligation["obligationDate"],
+                        "person_name": obligation["obligedPersonName"],
+                        "person_identifier": obligation["obligedPersonIdent"],
+                        "discount": obligation["additionalData"]["discount"],
+                    }
+                )
 
 
 class KatError(Exception):
@@ -49,32 +81,37 @@ async def async_verify_credentials(egn: str, license_number: str) -> bool:
         if license_match is None:
             raise ValueError("Driving License Number not valid")
 
-    return _verify_credentials_request(egn, license_number)
-
-
-async def async_check_obligations(
-    egn: str, license_number: str
-) -> KatObligationsResponse:
-    """Check obligations"""
-
-    data = _get_obligations_request(egn, license_number)
-
-    return KatObligationsResponse(data[_HAS_NON_HANDED_SLIP])
-
-
-async def _verify_credentials_request(egn: str, license_number: str) -> bool:
-    """Checks for valid credentials"""
     try:
         url = _KAT_OBLIGATIONS_URL.format(egn=egn, license_number=license_number)
 
         async with httpx.AsyncClient() as client:
-            resp = await client.get(url)
+            resp = await client.get(url, timeout=_REQUEST_TIMEOUT)
             resp.raise_for_status()
 
     except httpx.HTTPError:
         return False
 
     return True
+
+
+async def async_check_obligations(
+    egn: str, license_number: str
+) -> KatObligationsResponse:
+    """Check if the person has obligations"""
+
+    data = _get_obligations_request(egn, license_number)
+
+    return KatObligationsSimpleResponse(data)
+
+
+async def async_get_obligations(
+    egn: str, license_number: str
+) -> KatObligationsResponse:
+    """Get all obligations"""
+
+    data = _get_obligations_request(egn, license_number)
+
+    return KatObligationsResponse(data)
 
 
 async def _get_obligations_request(egn: str, license_number: str) -> any:
@@ -85,14 +122,20 @@ async def _get_obligations_request(egn: str, license_number: str) -> any:
     :param driving_license_number: Driver License Number
 
     """
+    data = {}
+
     try:
         url = _KAT_OBLIGATIONS_URL.format(egn=egn, license_number=license_number)
 
         async with httpx.AsyncClient() as client:
-            resp = await client.get(url)
+            resp = await client.get(url, timeout=_REQUEST_TIMEOUT)
             resp.raise_for_status()
 
         data = resp.json()
+
+    except httpx.TimeoutException as ex:
+        # The requests timeout from time to time, don't worry about it
+        raise KatError(f"KAT_BG: Request timed out for {license_number}") from ex
 
     except httpx.HTTPError as ex:
         if "code" in data:
@@ -114,11 +157,7 @@ async def _get_obligations_request(egn: str, license_number: str) -> any:
                 f"KAT_BG: Website returned an unknown error: {str(ex)}"
             ) from ex
 
-    except httpx.TimeoutException as ex:
-        # The requests timeout from time to time, don't worry about it
-        raise KatError(f"KAT_BG: Request timed out for {license_number}") from ex
-
-    if _HAS_NON_HANDED_SLIP not in data:
+    if _RESP_HAS_NON_HANDED_SLIP not in data or _RESP_OBLIGATIONS not in data:
         # This should never happen. If we go in this if, this probably means they changed their schema
         raise KatFatalError(f"KAT_BG: Website returned a malformed response: {data}")
 
